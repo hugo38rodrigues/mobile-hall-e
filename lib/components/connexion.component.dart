37eba1eb28441ae9be53ae0b70f90292c87aa03c
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2025-02-24 18:32:02 +0100

    delete useless component

--- lib/components/connexion.component.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Connexion extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Connexion'));
-  }
-}
