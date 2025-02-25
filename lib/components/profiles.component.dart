5859b05d5be7e6bb7ef6a6ad09914672dc2d0055
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2025-02-25 16:29:41 +0100

    add manage and profile page

--- lib/components/profiles.component.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Profiles extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Profiles'));
-  }
-}
--- lib/screen/profile.screen.dart
+++ lib/screen/profile.screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hall_e_mobile/components/logins/logins-wrapper.logins.dart';
-import 'package:hall_e_mobile/components/profiles.component.dart';
+import 'package:hall_e_mobile/components/profiles/profile.dart';
 import 'package:hall_e_mobile/providers/account.providers.dart';
 import 'package:hall_e_mobile/styles/font-colors.dart';
 
@@ -9,12 +9,12 @@ class ProfileScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final profile = ref.watch(accountProvider);
-    String role = profile['role'];
+    String role = profile.role;
     // Lire la valeur
     return Scaffold(
         backgroundColor: primaryColor,
         body: Column(
-          children: [role == 'invité' ? LoginsWrapper() : Profiles()],
+          children: [role == 'invité' ? LoginsWrapper() : Profile()],
         ));
   }
 }
