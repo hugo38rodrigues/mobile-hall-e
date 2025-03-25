a30dc30adf9942b8fe169d9f4542849a23464940
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2025-03-25 11:28:19 +0100

    delet useless button and add background color

--- lib/screen/bar.screen.dart
+++ lib/screen/bar.screen.dart
@@ -5,40 +5,20 @@ import 'package:hall_e_mobile/styles/font-colors.dart';
 class BarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 130,
-                  height: 40,
-                  child: ElevatedButton(
-                      onPressed: () => {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: secondaryColor),
-                      child: Text(
-                        'Map',
-                        style: TextStyle(color: primaryColor, fontSize: 18),
-                      )),
-                ),
-              ],
+    return Scaffold(
+        backgroundColor: primaryColor,
+        body: CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return BarList();
+                },
+                childCount:
+                    1, // Tu peux mettre le nombre d'éléments dans la liste ici
+              ),
             ),
-          ),
-        ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (context, index) {
-              return BarList();
-            },
-            childCount:
-                1, // Tu peux mettre le nombre d'éléments dans la liste ici
-          ),
-        ),
-      ],
-    ));
+          ],
+        ));
   }
 }
