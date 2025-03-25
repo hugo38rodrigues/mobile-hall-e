765202c985cf4f7ff5d9b5aff1c2ab4efb494d13
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2025-03-25 11:28:33 +0100

    change secondary color

--- lib/styles/font-colors.dart
+++ lib/styles/font-colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const Color primaryColor75 = Color.fromRGBO(242, 232, 220, 0.75);
 const Color primaryColor50 = Color.fromRGBO(242, 232, 220, 0.5);
 const Color primaryColor = Color.fromRGBO(242, 232, 220, 1);
-const Color secondaryColor = Colors.brown;
+const Color secondaryColor = Color.fromRGBO(152, 116, 100, 1);
 const TextTheme textThemes = TextTheme(
   bodyMedium: TextStyle(
     fontFamily: 'Lexend',
